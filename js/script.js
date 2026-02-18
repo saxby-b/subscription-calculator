13dc184f7b10390d290863d31207fc4a52ff1915
C. Saxby Bain <csaxbybain@yahoo.com>
2026-02-18 14:42:51 -0500

    Formatted code

--- js/script.js
+++ js/script.js
@@ -5,25 +5,23 @@ var subType = "basic";
 var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
-    subType = e.target.value;
-    updateSubscriptionDiv();
+  subType = e.target.value;
+  updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function (e) {
-    subDuration = Number(e.target.value);
-    updateSubscriptionDiv();
+  subDuration = Number(e.target.value);
+  updateSubscriptionDiv();
 });
 
 var updateSubscriptionDiv = function () {
-   var monthlyCost = 5;
+  var monthlyCost = 5;
 
-   if (subType === "standard") {
+  if (subType === "standard") {
     monthlyCost = 7;
-   } else if (subType === "premium") {
+  } else if (subType === "premium") {
     monthlyCost = 10;
-   }
-   var total = subDuration * monthlyCost;
-   result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
-
-}
-
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
